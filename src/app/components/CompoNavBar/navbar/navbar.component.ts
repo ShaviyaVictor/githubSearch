eeb7d927569bbdc7c005043f3322f27bd7dcec71
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-15 16:11:13 +0300

    Make imports to the nav component to facilitate data inflow

--- src/app/components/CompoNavBar/navbar/navbar.component.ts
+++ src/app/components/CompoNavBar/navbar/navbar.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Profile } from 'src/app/classes/ClassProfile/profile';
+import { ProfileService } from 'src/app/services/ServProfile/profile.service';
+import { environment } from 'src/environments/environment';
+import {  Observable  } from 'rxjs';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
--- src/app/services/ServProfile/profile.service.ts
+++ src/app/services/ServProfile/profile.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {  HttpClient, HttpClientModule  } from '@angular/common/http';
 
 
-import {  Observable  } from 'rxjs/Rx';
+import {  Observable  } from 'rxjs';
 import { Profile } from 'src/app/classes/ClassProfile/profile';
 import { environment } from 'src/environments/environment';
 
@@ -15,7 +15,7 @@ import { environment } from 'src/environments/environment';
 
 export class ProfileService {
 
-  profile:Profile [];
+  profile:Profile[] | undefined;
   userName:string = 'ShaviyaVictor';
   
 
@@ -26,7 +26,7 @@ export class ProfileService {
 
    getProfile(username:string): Observable<Profile[]> {
 
-    return this.http.get<Profile[]>(  environment.profUrl + this.userName  )
+    return this.http.get<Profile[]>(  environment.profUrl + this.userName  );
 
    }
    
