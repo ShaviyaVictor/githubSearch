1b4e86581387b85ca76ec9861f2e261d463b8a35
ShaviyaVictor <shaviyavictor@gmail.com>
2022-01-18 06:21:57 +0300

    Add the client id and client secret for unlimited access to the API

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# QuoteHub
+# githubSearch
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.4.     
 [github_Search_Screenshot](#)
--- src/app/services/ServProfile/profile.service.ts
+++ src/app/services/ServProfile/profile.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import {  HttpClient, HttpClientModule  } from '@angular/common/http';
+import {  HttpClient, HttpClientModule, HttpHeaders  } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 
 import {  Observable  } from 'rxjs';
@@ -16,11 +17,15 @@ import { environment } from 'src/environments/environment';
 export class ProfileService {
 
   profile:Profile[] | undefined;
-  userName:string = 'ShaviyaVictor';
+  private userName:string;
+  private clientId = '2b6a1427f546a62194a1';
+  private clientSecret = 'ab2ee006ff37ad17b7434695d405ce3f9ad52a58';
   
 
   constructor(  private http:HttpClient ) { 
 
+    console.log('Service is now running')
+    this.userName = 'ShaviyaVictor';
 
    }
 
